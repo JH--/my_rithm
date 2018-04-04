3f956eede530be1d01e14978ae36dc0728647cc1
Joe Hirsch <jhirsch2914@hotmail.com>
2018-04-04 01:36:13 -0700

    add items to table

---
+++ intermediate_javaScript_part_2/jQuery_list_page/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+	$("#movie").submit( event => {
+		event.preventDefault();
+		let title = ($("#movieTitle").val() ? $("#movieTitle").val() : false);
+		if (!title){
+			return;
+		}
+		let rating = $("#movieRating").val();
+		rating = (rating >= 1 && rating <= 10 ? rating : 10); 
+		let $row = $("<tr>");
+		$row
+		  .append($("<td>", {
+		  	class: "text-center align-middle",
+		    text: title
+		  }))
+		  .append($("<td>", {
+		  	class: "text-center align-middle",
+		  	text: rating
+		  }))
+		  .append($("<td>", {
+		  	class: "text-center"
+		  }).append($("<button>", {
+		  	type: "button",
+		  	class: "btn btn-danger",
+		  	id: "delete",
+		  	text: "Delete"
+		  })))
+		$("tbody").append($row);
+		$("#movieTitle").val("");
+		$("#movieRating").val("");
+	})
+})
+
+/*
+<tr>
+	<td class="text-center align-middle">Star Wars The Last Jedi</td>
+	<td class="text-center align-middle">8</td>
+	<td class="text-center"><button type="button" class="btn btn-danger" id="delete">Delete</button></td>
+</tr>
+*/
