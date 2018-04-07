879f5aaa79a0a93650be5a8adce7e336a09ca599
Joe Hirsch <jhirsch2914@hotmail.com>
2018-04-07 01:08:52 -0700

    giphy party app

--- intermediate_javaScript_part_2/ajax_exercise/app.js
+++ intermediate_javaScript_part_2/ajax_exercise/app.js
@@ -1,3 +1,17 @@
 $(document).ready(() => {
-  
+  $("form").on("submit", event => {
+    event.preventDefault();
+    const query = ($("#query").val() ? $("#query").val() : false);
+    if(!query){return;}
+    const giphyAPI = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC`;
+    $.getJSON(giphyAPI).done(response => {
+      const gif = response.data[Math.floor(Math.random() * response.data.length)].images.downsized.url;
+      const $gif = $("<div>", {class:"col-auto my-1"}).append($("<img>", {src:gif}));
+      $("#gifCity").append($gif);
+    })
+    .always( () => $("#query").val(''));
+  })
+
+  $("#delete").on("click", e => $("#gifCity").empty());
 })
+
