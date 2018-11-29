672adb13102d3576224980c028254d5a77166333
JH-- <jhirsch2914@hotmail.com>
2018-11-28 20:54:07 -0800

    user index

--- flask_intermediate/blueprints_user_message_app/project/users/views.py
+++ flask_intermediate/blueprints_user_message_app/project/users/views.py
@@ -1,5 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from project.models import User
 
 users_blueprint = Blueprint("users", __name__, template_folder="templates")
+
+
+@users_blueprint.route("/", methods=["GET"])
+def index():
+    return render_template("index.html", users=User.query.all())
 
