4ac5c0ae495ed831ef3606c4e7c4f64499218974
JH-- <jhirsch2914@hotmail.com>
2018-12-06 23:52:16 -0800

    clean up

--- flask_intermediate/user_message_app_blueprints/project/messages/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, validators
-
-
-class MessageForm(FlaskForm):
-    content = StringField("New Message", [validators.DataRequired()])
