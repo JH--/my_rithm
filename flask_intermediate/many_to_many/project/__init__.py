d2d94243dc6472174a9efa716d321758405cfe0f
JH-- <jhirsch2914@hotmail.com>
2018-12-14 01:58:35 -0800

    set up new employee form and route

--- flask_intermediate/many_to_many/project/__init__.py
+++ flask_intermediate/many_to_many/project/__init__.py
@@ -1,10 +1,18 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_modus import Modus
+from flask_wtf.csrf import CSRFProtect
+from os import environ
 
 app = Flask(__name__)
+modus = Modus(app)
+csrf = CSRFProtect(app)
+
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///many_to_many_example_sqlalchemy"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -15,6 +23,13 @@ app.register_blueprint(employees_blueprint, url_prefix="/employees")
 from project.departments.views import departments_blueprint
 
 app.register_blueprint(departments_blueprint, url_prefix="/departments")
+
+
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+app.register_error_handler(404, page_not_found)
 
 
 @app.route("/")
--- flask_intermediate/many_to_many/project/employees/views.py
+++ flask_intermediate/many_to_many/project/employees/views.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, redirect, render_template, request, url_for
-from project.models import Employee
+from flask_wtf.csrf import validate_csrf, ValidationError
+from project.employees.forms import NewEmployeeForm
+from project.models import Employee, Department
+from project import db
 
 employees_blueprint = Blueprint("employees", __name__, template_folder="templates")
 
@@ -7,3 +10,30 @@ employees_blueprint = Blueprint("employees", __name__, template_folder="template
 @employees_blueprint.route("/", methods=["GET"])
 def index():
     return render_template("employees/index.html", employees=Employee.query.all())
+
+
+@employees_blueprint.route("/new", methods=["GET"])
+def new():
+    form = NewEmployeeForm()
+    form.set_choices()
+    return render_template("employees/new.html", form=form)
+
+
+@employees_blueprint.route("/", methods=["POST"])
+def new_employee():
+    form = NewEmployeeForm(request.form)
+    form.set_choices()
+    if form.validate():
+        employee = Employee(request.form["name"], request.form["years_at_company"])
+        db.session.add(employee)
+        for department in form.departments.data:
+            d = Department.query.get(department)
+            d.employees.extend([employee])
+        db.session.commit()
+        return redirect(url_for("employees.index"))
+    return redirect(url_for("employees.new"))
+
+
+@employees_blueprint.route("/<int:id>", methods=["GET"])
+def show(id):
+    return
