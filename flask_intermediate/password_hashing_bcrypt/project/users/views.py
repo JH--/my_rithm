d168e248a1b13b8faca44f06f9a028efd0de584f
JH-- <jhirsch2914@hotmail.com>
2019-04-06 00:26:33 -0700

    password exercise

--- flask_intermediate/password_hashing_bcrypt/project/users/views.py
+++ flask_intermediate/password_hashing_bcrypt/project/users/views.py
@@ -1,10 +1,43 @@
-from flask import redirect, render_template, request, url_for, Blueprint
+from flask import (
+    redirect,
+    render_template,
+    request,
+    url_for,
+    Blueprint,
+    session,
+    flash,
+    g,
+)
 from project.users.forms import UserForm
 from project.users.models import User
 from project import db
 from sqlalchemy.exc import IntegrityError
+from functools import wraps
+
 
 users_blueprint = Blueprint("users", __name__, template_folder="templates")
+
+
+def ensure_logged_in(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if not session.get("user_id"):
+            flash("Please log in first")
+            return redirect(url_for("users.login"))
+        return fn(*args, **kwargs)
+
+    return wrapper
+
+
+def ensure_correct_user(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if kwargs.get("id") != session.get("user_id"):
+            flash("Not Authorized")
+            return redirect(url_for("users.welcome"))
+        return fn(*args, **kwargs)
+
+    return wrapper
 
 
 @users_blueprint.route("/signup", methods=["GET", "POST"])
@@ -16,6 +49,7 @@ def signup():
             db.session.add(new_user)
             db.session.commit()
         except IntegrityError as e:
+            flash("Invalid submission. Please try again")
             return render_template("signup.html", form=form)
         return redirect(url_for("users.login"))
     return render_template("signup.html", form=form)
@@ -25,12 +59,47 @@ def signup():
 def login():
     form = UserForm(request.form)
     if request.method == "POST" and form.validate():
-        if User.authenticate(form.data["username"], form.data["password"]):
+        user = User.authenticate(form.data["username"], form.data["password"])
+        if user:
+            session["user_id"] = user.id
+            flash("You've successfully logged in!")
             return redirect(url_for("users.welcome"))
     return render_template("login.html", form=form)
 
 
+@users_blueprint.route("/logout")
+def logout():
+    session.pop("user_id", None)
+    flash("You have been signed out")
+    return redirect(url_for("users.login"))
+
+
 @users_blueprint.route("/welcome")
+@ensure_logged_in
 def welcome():
     return render_template("welcome.html")
+    # return render_template("welcome.html", user=g.current_user)
+
+
+@users_blueprint.route("/<int:id>")
+@ensure_logged_in
+def show(id):
+    user = User.query.get(id)
+    if not user:
+        return redirect(url_for("users.welcome"))
+    return render_template("show.html", user=user)
+
+
+@users_blueprint.route("/<int:id>/edit")
+@ensure_correct_user
+def edit(id):
+    return render_template("edit.html", user=User.query.get(id))
+
+
+@users_blueprint.before_request
+def current_user():
+    if session.get("user_id"):
+        g.current_user = User.query.get(session["user_id"])
+    else:
+        g.current_user = None
 
